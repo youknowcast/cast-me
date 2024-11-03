cc44b1325b12cb15a2a292246fa6bfa3d03a3792
Ambe Hideki <ykc@daycrift.net>
2024-11-04 03:45:31 +0900

    Feat: Refactor calendar controller and view
    Refactor `CalendarController` to extract date logic and generate calendar weeks. Update view from ERB to Slim and improve layout by displaying the calendar and incorporating event add buttons.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,6 +2,22 @@ class CalendarController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @date = date
+    @weeks = generate_calendar_weeks
+  end
+
+  private
+
+  def date = @_date ||= params[:date] ? Date.parse(params[:date]) : Date.today
+
+  def generate_calendar_weeks
+    first_day = date.beginning_of_month
+    last_day = date.end_of_month
+
+    # カレンダーの最初の日と最後の日を設定（日曜日始まり）
+    start_date = first_day - first_day.wday
+    end_date = last_day + (6 - last_day.wday)
+
+    (start_date..end_date).to_a.in_groups_of(7) # 7日ごとの配列
   end
 end
