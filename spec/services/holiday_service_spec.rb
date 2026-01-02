714cf091d590001de129ea5e9a6dee64029f87b0
Hideki Ambe <ykc@daycrift.net>
2026-01-02 23:27:02 +0900

    Refactor: fix spec that rubocop specifies

--- spec/services/holiday_service_spec.rb
+++ spec/services/holiday_service_spec.rb
@@ -5,37 +5,35 @@ RSpec.describe HolidayService, type: :service do
     let(:current_month) { Time.current.strftime('%Y-%m') }
     let(:cache_key) { "holidays-#{current_month}" }
     let(:api_response_body) { { '2026-01-01' => '元日' }.to_json }
+    let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+    let(:http_double) { instance_double(Net::HTTP) }
+    let(:response_double) { instance_double(Net::HTTPResponse, body: api_response_body) }
 
     before do
       # Mock cache to memory store for testing caching logic
-      @memory_store = ActiveSupport::Cache.lookup_store(:memory_store)
-      allow(Rails).to receive(:cache).and_return(@memory_store)
+      allow(Rails).to receive(:cache).and_return(memory_store)
       Rails.cache.clear
 
       # Mock Net::HTTP
-      @http_double = instance_double(Net::HTTP)
-      allow(Net::HTTP).to receive(:new).and_return(@http_double)
-      allow(@http_double).to receive(:use_ssl=)
-      allow(@http_double).to receive(:verify_mode=)
-
-      @response_double = instance_double(Net::HTTPResponse, body: api_response_body)
-      allow(@http_double).to receive(:get).and_return(@response_double)
+      allow(Net::HTTP).to receive(:new).and_return(http_double)
+      allow(http_double).to receive(:use_ssl=)
+      allow(http_double).to receive(:verify_mode=)
+      allow(http_double).to receive(:get).and_return(response_double)
     end
 
     it 'fetches holidays from the API and caches them' do
-      expect(Net::HTTP).to receive(:new).once
-
       holidays = described_class.holidays
+
+      expect(Net::HTTP).to have_received(:new).once
       expect(holidays).to eq({ '2026-01-01' => '元日' })
       expect(Rails.cache.exist?(cache_key)).to be true
     end
 
     it 'uses the cache on subsequent calls' do
       described_class.holidays
-      expect(Net::HTTP).not_to receive(:new)
+      described_class.holidays
 
-      holidays = described_class.holidays
-      expect(holidays).to eq({ '2026-01-01' => '元日' })
+      expect(Net::HTTP).to have_received(:new).once
     end
 
     context 'when the API returns empty data' do
@@ -49,7 +47,7 @@ RSpec.describe HolidayService, type: :service do
 
     context 'when an error occurs' do
       before do
-        allow(@http_double).to receive(:get).and_raise(StandardError.new('API Error'))
+        allow(http_double).to receive(:get).and_raise(StandardError.new('API Error'))
       end
 
       it 'returns an empty hash and does not crash' do
