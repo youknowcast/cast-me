c917241f8a60cbdfaefd42e905f6fab51886e88f
Hideki Ambe <ykc@daycrift.net>
2026-01-02 20:23:49 +0900

    refactor: standardize date creation to `Time.zone.today` and use single quotes in task specs and factory.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task do
     association :family
     association :user
-    title { "Test Task" }
-    date { Date.today }
+    title { 'Test Task' }
+    date { Time.zone.today }
     priority { 1 }
     completed { false }
   end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :request do
+RSpec.describe 'Tasks', type: :request do
   let(:family) { create(:family) }
   let(:user) { create(:user, family: family) }
-  let(:task) { create(:task, family: family, user: user, date: Date.today, completed: false) }
+  let(:task) { create(:task, family: family, user: user, date: Time.zone.today, completed: false) }
 
   before do
     sign_in user
   end
 
-  describe "PATCH /update" do
-    it "updates the completed status" do
+  describe 'PATCH /update' do
+    it 'updates the completed status' do
       patch task_path(task), params: { task: { completed: true } }
       task.reload
       expect(task.completed).to be true
     end
 
-    it "updates other attributes" do
-      patch task_path(task), params: { task: { title: "New Title" } }
+    it 'updates other attributes' do
+      patch task_path(task), params: { task: { title: 'New Title' } }
       task.reload
-      expect(task.title).to eq("New Title")
+      expect(task.title).to eq('New Title')
     end
   end
 end
