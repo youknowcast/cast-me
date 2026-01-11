0dad2ee7db3de17ca23c0e84e04d4a19a8c56d78
youknowcast <ykc@daycrift.net>
2026-01-12 03:29:10 +0900

    Manually add schema information to Article related models
    - Add schema info to Article, Tag, and ArticleTag as annotate gem is incompatible with Rails 8

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :integer          not null, primary key
+#  pinned      :boolean          default(FALSE), not null
+#  title       :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
 class Article < ApplicationRecord
   belongs_to :user
   has_many :article_tags, dependent: :destroy
